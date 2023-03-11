73560e93a2f7244312b9436dfa47247de0023055
Nimrod Cinman <nimrodcinman@gmail.com>
2023-03-12 00:02:46 +0200

    more styling

--- src/components/PostContent/index.tsx
+++ src/components/PostContent/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import { Post } from 'src/types';
 
 import './styles.scss';
@@ -22,6 +23,9 @@ const PostContent: FC<PostContentProps> = ({ post }) => {
 
   return (
     <section id="post-content">
+      <Link to="/blog/" id="button-back-to-blog">
+        {'<- Back to blog page'}
+      </Link>
       <h2>{post.mainTitle}</h2>
       <h4>{post.summary}</h4>
       {paragraphs}
